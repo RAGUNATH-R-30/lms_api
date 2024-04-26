c3f0692da40ae82cb4f22dcde1edb5fab614494e
RAGUNATH-R-30 <ragunath3003@gmail.com>
2024-04-26 12:36:44 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello World")
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+console.log("connecting to mongodb...");
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log("connected to mongodb..");
+  })
+  .catch((error) => {
+    console.log("Error connecting to mongodb", error.message);
+  });
