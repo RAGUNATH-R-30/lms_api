c8f52847e9fad6166e55863b789885c3762718ed
RAGUNATH-R-30 <ragunath3003@gmail.com>
2024-04-26 12:49:03 +0530

    server started

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
 const config = require("./utils/config");
+const app = require("./app")
+
 console.log("connecting to mongodb...");
+
+
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
     console.log("connected to mongodb..");
+    app.listen(config.PORT,()=>{
+        console.log(`Server Running on port ${config.PORT}`)
+      })
   })
   .catch((error) => {
     console.log("Error connecting to mongodb", error.message);
   });
+
+
