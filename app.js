6c3bd9dce7023fb49658872d6102fec11007ea5d
RAGUNATH-R-30 <ragunath3003@gmail.com>
2024-05-09 22:35:12 +0530

    cors update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cookieParser = require('cookie-parser');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'*',
     credentials:true
 }))
 app.use(cookieParser())
